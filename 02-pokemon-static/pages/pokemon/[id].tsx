cf27a3b240f2246908045cce6f026917223d0416
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2022-09-28 23:34:58 -0500

    54 .- Diseño de la pagina pokemon

--- 02-pokemon-static/pages/pokemon/[id].tsx
+++ 02-pokemon-static/pages/pokemon/[id].tsx
@@ -1,5 +1,5 @@
+import { Button, Card, Container, Grid, Text, Image } from "@nextui-org/react";
 import { NextPage, GetStaticProps, GetStaticPaths } from "next";
-import { useRouter } from "next/router";
 import { pokeApi } from "../../api";
 import { Layout } from "../../components/layout";
 import { Pokemon } from "../../interfaces";
@@ -9,9 +9,70 @@ interface Props {
 }
 
 const PokemonPage: NextPage<Props> = ({ pokemon }) => {
-   const { query } = useRouter();
-
-   return <Layout title="Pokemon">{pokemon.name}</Layout>;
+   return (
+      <Layout title="Pokemon">
+         <Grid.Container css={{ marginTop: "5px" }} gap={2}>
+            <Grid xs={12} md={4}>
+               <Card isHoverable css={{ padding: "30px" }}>
+                  <Card.Body>
+                     <Card.Image
+                        src={
+                           pokemon.sprites.other?.dream_world.front_default ||
+                           "/no-image.png"
+                        }
+                        alt={pokemon.name}
+                        width="100%"
+                        height={200}
+                     />
+                  </Card.Body>
+               </Card>
+            </Grid>
+            <Grid xs={12} sm={8}>
+               <Card>
+                  <Card.Header
+                     css={{ display: "flex", justifyContent: "space-between" }}
+                  >
+                     <Text h1 transform="capitalize">
+                        {pokemon.name}
+                     </Text>
+                     <Button color="gradient" ghost>
+                        Guardar en Favoritos
+                     </Button>
+                  </Card.Header>
+                  <Card.Body>
+                     <Text size={30}>Sprites:</Text>
+                     <Container display="flex" direction="row" gap={0}>
+                        <Image
+                           src={pokemon.sprites.front_default}
+                           alt={pokemon.name}
+                           width={100}
+                           height={100}
+                        />
+                        <Image
+                           src={pokemon.sprites.back_default}
+                           alt={pokemon.name}
+                           width={100}
+                           height={100}
+                        />
+                        <Image
+                           src={pokemon.sprites.front_shiny}
+                           alt={pokemon.name}
+                           width={100}
+                           height={100}
+                        />
+                        <Image
+                           src={pokemon.sprites.back_shiny}
+                           alt={pokemon.name}
+                           width={100}
+                           height={100}
+                        />
+                     </Container>
+                  </Card.Body>
+               </Card>
+            </Grid>
+         </Grid.Container>
+      </Layout>
+   );
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
