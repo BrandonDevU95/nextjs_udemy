bf8db4970148ae5bba84eadd3b5a1764d28f2558
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2022-09-28 23:00:08 -0500

    52 .- GetStaticPaths

--- 02-pokemon-static/pages/pokemon/[id].tsx
+++ 02-pokemon-static/pages/pokemon/[id].tsx
@@ -1,14 +1,43 @@
+import { NextPage, GetStaticProps, GetStaticPaths } from "next";
 import { useRouter } from "next/router";
 import { Layout } from "../../components/layout";
 
 interface Props {
-   pokemon: any;
+   //    pokemon: any;
+   id: string;
+   name: string;
 }
 
-const PokemonPage = () => {
+const PokemonPage: NextPage<Props> = ({ id, name }) => {
    const { query } = useRouter();
 
-   return <Layout title="Pokemon">PokemonPage</Layout>;
+   return (
+      <Layout title="Pokemon">
+         {id} - {name}
+      </Layout>
+   );
+};
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+   return {
+      paths: [
+         {
+            params: {
+               id: "1",
+            },
+         },
+      ],
+      fallback: false,
+   };
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+   return {
+      props: {
+         id: 1,
+         name: "Bulbasaur",
+      },
+   };
 };
 
 export default PokemonPage;
