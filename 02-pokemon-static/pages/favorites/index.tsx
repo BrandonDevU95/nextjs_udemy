ea501023ee068af44fbc4a0709e0ee579e2718e7
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2022-10-01 13:38:46 -0500

    66 .- Listado de avoritos

--- 02-pokemon-static/pages/favorites/index.tsx
+++ 02-pokemon-static/pages/favorites/index.tsx
@@ -1,3 +1,4 @@
+import { Card, Grid } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import { Layout } from "../../components/layout";
 import { NoFavorites } from "../../components/ui";
@@ -12,7 +13,23 @@ const FavoritePage = () => {
 
    return (
       <Layout title="Pokemon">
-         <NoFavorites />
+         {favoritePokemos.length === 0 ? (
+            <NoFavorites />
+         ) : (
+            <Grid.Container gap={2} direction="row" justify="flex-start">
+               {favoritePokemos.map((pokemonId) => (
+                  <Grid xs={12} sm={6} md={4} lg={2} key={pokemonId}>
+                     <Card isHoverable isPressable css={{ padding: 10 }}>
+                        <Card.Image
+                           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonId}.svg`}
+                           width={"100%"}
+                           height={140}
+                        />
+                     </Card>
+                  </Grid>
+               ))}
+            </Grid.Container>
+         )}
       </Layout>
    );
 };
