dc4e98f1adad7599e3d8097f5c8675cd274a35f3
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2022-10-11 22:04:39 -0500

    180 - HomePage

--- 05-Teslo-Shop/pages/index.tsx
+++ 05-Teslo-Shop/pages/index.tsx
@@ -1,6 +1,13 @@
-import { Typography } from "@mui/material";
+import {
+   Card,
+   CardActionArea,
+   CardMedia,
+   Grid,
+   Typography,
+} from "@mui/material";
 import type { NextPage } from "next";
 import { ShopLayout } from "../components/layout";
+import { initialData } from "../database/products";
 
 const Home: NextPage = () => {
    return (
@@ -14,6 +21,21 @@ const Home: NextPage = () => {
          <Typography variant="h2" sx={{ mb: 1 }} component="h2">
             Todos los productos
          </Typography>
+         <Grid container spacing={4}>
+            {initialData.products.map((product) => (
+               <Grid item xs={6} sm={4} key={product.slug}>
+                  <Card>
+                     <CardActionArea>
+                        <CardMedia
+                           component="img"
+                           image={`products/${product.images[0]}`}
+                           alt={product.title}
+                        />
+                     </CardActionArea>
+                  </Card>
+               </Grid>
+            ))}
+         </Grid>
       </ShopLayout>
    );
 };
