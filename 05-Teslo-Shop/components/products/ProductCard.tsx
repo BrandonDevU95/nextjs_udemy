aa19fe6133e1f96c1b0804431866d0435c545436
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2022-10-12 19:48:03 -0500

    184 - Pantalla de producto

--- 05-Teslo-Shop/components/products/ProductCard.tsx
+++ 05-Teslo-Shop/components/products/ProductCard.tsx
@@ -4,10 +4,12 @@ import {
    CardMedia,
    Grid,
    Box,
+   Link,
    Typography,
 } from "@mui/material";
 import { FC, useMemo, useState } from "react";
 import { IProduct } from "../../interfaces";
+import NextLink from "next/link";
 
 interface Props {
    product: IProduct;
@@ -31,14 +33,18 @@ export const ProductCard: FC<Props> = ({ product }) => {
          onMouseLeave={() => setIsHovered(false)}
       >
          <Card>
-            <CardActionArea>
-               <CardMedia
-                  component="img"
-                  className="fadeIn"
-                  image={productImage}
-                  alt={product.title}
-               />
-            </CardActionArea>
+            <NextLink href={`/product/slug`} passHref prefetch={false}>
+               <Link>
+                  <CardActionArea>
+                     <CardMedia
+                        component="img"
+                        className="fadeIn"
+                        image={productImage}
+                        alt={product.title}
+                     />
+                  </CardActionArea>
+               </Link>
+            </NextLink>
          </Card>
 
          <Box sx={{ mt: 1 }} className="fadeIn">
