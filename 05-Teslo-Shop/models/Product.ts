7ccc21cd5bcf50fe8c607998c221c579d755ecaf
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2022-10-13 20:24:27 -0500

    204 - Mongoose y modelo de producto

---
+++ 05-Teslo-Shop/models/Product.ts
@@ -0,0 +1,23 @@
+import mongoose, {Schema, model, Model} from 'mongoose'
+import { IProduct } from '../interfaces'
+
+const productSchema = new Schema({
+    description: {type: String, required: true},
+    images: [{type: String}],
+    inStock: {type: Number, required: true, default: 0},
+    price: {type: Number, required: true, default: 0},
+    sizes: [{type: String, enum: ['XS','S', 'M', 'L', 'XL','XXL','XXXL'], message: 'Size is not valid'}],
+    slug: {type: String, required: true, unique: true},
+    tags: [{type: String}],
+    title: {type: String, required: true},
+    type: {type: String, enum: ['shirt', 'pants', 'shoes', 'accessories'], message: 'Type is not valid'},
+    gender: {type: String, enum: ['men', 'women', 'kid', 'unisex'], message: 'Gender is not valid' }
+},{
+    timestamps: true
+})
+
+//TODO: Crear un Indice
+
+const Product: Model<IProduct> = mongoose.models.Product || model('Product', productSchema)
+
+export default Product
