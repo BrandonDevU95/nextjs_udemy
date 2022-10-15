6ce0875023360b3242066b3349e7ab63dd6d758d
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2022-10-14 20:58:32 -0500

    216 - SWR Fetching

--- 05-Teslo-Shop/pages/index.tsx
+++ 05-Teslo-Shop/pages/index.tsx
@@ -1,10 +1,18 @@
 import { Typography } from "@mui/material";
 import type { NextPage } from "next";
+import useSWR from "swr";
 import { ShopLayout } from "../components/layout";
 import { ProductList } from "../components/products";
-import { initialData } from "../database/products";
+
+const fetcher = (...args: [key: string]) =>
+   fetch(...args).then((res) => res.json());
 
 const Home: NextPage = () => {
+   const { data, error } = useSWR("/api/products", fetcher);
+
+   if (error) return <div>failed to load</div>;
+   if (!data) return <div>loading...</div>;
+
    return (
       <ShopLayout
          title="Teslo-Shop - Home"
@@ -16,7 +24,7 @@ const Home: NextPage = () => {
          <Typography variant="h2" sx={{ mb: 1 }} component="h2">
             Todos los productos
          </Typography>
-         <ProductList products={initialData.products as any} />
+         <ProductList products={data} />
       </ShopLayout>
    );
 };
